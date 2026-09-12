190a9b71694778b4e368be696c82e9925dbb7124
abidash081 <abidash081@gmail.com>
2026-09-12 13:44:05 +1000

    Update asyncHandler.js

--- server/asyncHandler.js
+++ server/asyncHandler.js
@@ -2,7 +2,7 @@
 // its own - wrap every handler with this so a thrown/rejected error reaches
 // next(err) (and our JSON error middleware) instead of hanging the request.
 module.exports = function asyncHandler(fn) {
-    return function (req, res, next) {
-          Promise.resolve(fn(req, res, next)).catch(next);
-    };
+  return function (req, res, next) {
+    Promise.resolve(fn(req, res, next)).catch(next);
+  };
 };
